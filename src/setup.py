0201d1cd6bcde7aee6c95f932013d19c45b0f38d
abehrens <61003964+abehrens13@users.noreply.github.com>
2025-11-02 09:36:17 +0100

    updates

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='discord_stroke_support_bot',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Andre',
+    author_email='',
+    description=''
+)
