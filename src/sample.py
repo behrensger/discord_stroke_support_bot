a84aab4e77b284a7b99401c228098a2399e7179e
abehrens <61003964+abehrens13@users.noreply.github.com>
2025-11-03 17:40:45 +0100

    updates

--- src/sample.py
+++ src/sample.py
@@ -1,39 +1,44 @@
 import os
 
 import discord
-import random
+import logging
+import datetime
 from dotenv import load_dotenv
+logger = logging.getLogger(__name__)
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(message):
+    async def on_message(self, message: discord.Message):
         username = str(message.author).split("#")[0]
         channel = str(message.channel.name)
-        user_message = str(message.content)
+        user_message = message.content.lower()
+        logger.info(f'Incoming Message {user_message} by {username} on {channel}')
 
-        print(f'Message {user_message} by {username} on {channel}')
-
-        #if message.author == client.user:
-        #    return
-
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
+        if message.author == client.user:
             return
-        elif user_message.lower() == "bye":
-            await message.channel.send(f'Bye {username}')
-        elif user_message.lower() == "tell me a joke":
-            jokes = [" Can someone please shed more\
-            light on how my lamp got stolen?",
-                     "Why is she called llene? She\
-                     stands on equal legs.",
-                     "What do you call a gazelle in a \
-                     lions territory? Denzel."]
-            await message.channel.send(random.choice(jokes))
 
+        if user_message.count("proto"):
+            split = user_message.split()
+            today = datetime.datetime.now()
+            if not os.path.isfile(f'{username}.csv'):
+                with open(f'{username}.csv', "a", encoding="utf-8") as f:
+                    f.write("username,today,systolic,diastolic\n")
+            with open(f'{username}.csv', "a", encoding="utf-8") as f:
+                f.write(f'{username},{today.isoformat()},{split[1].strip()}, {split[2].strip()}\n')
+            await message.channel.send(f'Protokoll: {split[1]}, {split[2]} and timestap: {today}')
+            logger.info(f'Protokoll: {username} for {split[1]}, {split[2]} and {today}')
+
+
+#############################################
+# inital load
 load_dotenv()
-client = MyClient(intents=discord.Intents.default())
+intents = discord.Intents.default() # create a default Intents instance
+intents.message_content = True # enable message content intents
+client = MyClient(intents= intents)
+logging.basicConfig(level=logging.INFO)
+
 token = os.getenv('DISCORD_TOKEN')
 if token is None:
     print('Token DISCORD_TOKEN not found!')
